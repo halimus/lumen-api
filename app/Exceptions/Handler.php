389a26dbaa048db295fc2575851b60c475eca8ce
LARDJANE <halim.lardjane@gmail.com>
2017-01-03 17:26:48 -0500

    trying to customize 404 page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -9,6 +9,10 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -45,6 +49,20 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        //return parent::render($request, $e);
+        
+//        if($e instanceof NotFoundHttpException){
+//            //return response(view("errors.404"), 404);
+//            return view("errors.404");
+//        }
+//        return parent::render($request, $e);
+        
+        if($e instanceof NotFoundHttpException){
+            //return response()->view('missing', [], 404);
+            //return Response::view('errors.missing', [], 404);
+            return \Response::view('errors.404',array(),404);
+        }
         return parent::render($request, $e);
+        
     }
 }
